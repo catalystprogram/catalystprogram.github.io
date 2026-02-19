f3263de43d742953cfbc1e5eaaa734276b9e81be
Catalyst Program <catalyst.program.board@gmail.com>
2026-02-19 17:06:56 -0500

    Removed Hero.css import from Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import './Hero.css';
 
 const Hero = () => {
     return (
         <div className="hero">
-            <h1>Welcome to Catalyst Program</h1>
-            <p>A student-led projects program involving students from universities accross the United States, including Berkeley, Purdue, and UChicago.</p>
+            <h1>Welcome to Our Site!</h1>
         </div>
     );
 };
