6e4ab7fd3a00200d12dc83d467358dd9e3cae50e
Catalyst Program <catalyst.program.board@gmail.com>
2026-02-19 16:41:50 -0500

    Simplify calendar overview by removing timeline elements

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,12 +1,1 @@
-import React from 'react';
-
-const Calendar = () => {
-  return (
-    <div>
-      <h1>Calendar Overview</h1>
-      <p>For information on specific dates, please ask your group organizer.</p>
-    </div>
-  );
-};
-
-export default Calendar;
+For information on specific dates, please ask your group organizer.
