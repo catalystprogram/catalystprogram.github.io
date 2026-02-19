7f9e1f54d9d9760aa7ac9ca5b990bdb9e6f4311b
Catalyst Program <catalyst.program.board@gmail.com>
2026-02-19 16:56:55 -0500

    Updated Hero component to remove CSS import and updated description text

---
+++ Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Hero = () => {
+    return (
+        <div>
+            <h1>Welcome to the Catalyst Program</h1>
+            <p>A student-led projects program involving students from universities accross the United States, including Berkeley, Purdue, and UChicago.</p>
+        </div>
+    );
+};
+
+export default Hero;
