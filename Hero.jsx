a85ccb8446ed5132f949c4de4a916bb9435d7ef5
Brian Foerster <aersling2430@gmail.com>
2026-02-19 17:49:08 -0500

    Restore full hero section and update top description text

--- Hero.jsx
+++ Hero.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Hero = () => {
     return (
         <div>
-            <h1>Welcome to the Catalyst Program</h1>
+            <h1>Catalyst Program</h1>
             <p>A student-led projects program involving students from universities accross the United States, including Berkeley, Purdue, and UChicago.</p>
         </div>
     );
